c3fac75a4e10ff007bf5ebb8052abc622c444280
VR <blopez3@saic.edu>
2020-07-03 14:18:39 -0500

    Update places.js

--- places.js
+++ places.js
@@ -32,7 +32,7 @@ function renderPlaces(places) {
         sign.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         //sign.setAttribute('href', 'http://www.example.com/');
         //sign.setAttribute('src', '../assets/map-marker.png'); // create a variable tto point to that sign
-        console.log("latitute" + ${latitude}); 
+        console.log("latitute" + latitude);
         sign.setAttribute('scale', '10 10 10');
 
         sign.setAttribute('look-at', '[gps-camera]');
